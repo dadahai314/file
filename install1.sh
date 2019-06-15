ee7c36d95438c052ce1f46e7a1f7911d2b0253e4
dadahai314 <38878824+dadahai314@users.noreply.github.com>
2019-06-15 13:42:41 +0800

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -1,6 +1,6 @@
 mkdir v2ray_do
 cd v2ray_do
-yum install net-tools wget curl -y
+#yum install net-tools wget curl -y
 
 wget https://raw.githubusercontent.com/828768/Shell/master/deploy_node.sh
 bash deploy_node.sh
