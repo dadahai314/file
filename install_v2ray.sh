565b0dc56f73d7a426292cec8bc7005f45c0dbc1
dadahai314 <38878824+dadahai314@users.noreply.github.com>
2019-07-24 23:31:01 +0800

    Create install_v2ray.sh

---
+++ install_v2ray.sh
@@ -0,0 +1,14 @@
+yum install -y wget curl net-tools unzip vim expect*
+mkdir down;cd down
+unzip master;cd file-master
+wget https://raw.githubusercontent.com/828768/Shell/master/deploy_node.sh
+bash deploy_node.sh
+
+bash <(curl -s -L https://233blog.com/v2ray.sh)
+
+
+uname -a
+rpm -vih http://soft.91yun.org/ISO/Linux/CentOS/kernel/kernel-3.10.0-229.1.2.el7.x86_64.rpm --force
+rpm -qa |grep kernel
+reboot
+
